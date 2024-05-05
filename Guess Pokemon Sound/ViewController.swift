e62f3ff841a7cb6244275828050cd9266cff2118
com.bonbon <chiweiju0@gmail.com>
2024-05-06 00:56:26 +0200

    Initial Commit

---
+++ Guess Pokemon Sound/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guess Pokemon Sound
+//
+//  Created by 紀韋如 on 06/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Guess Pokemon SoundUITests/Guess_Pokemon_SoundUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Guess_Pokemon_SoundUITestsLaunchTests.swift
+//  Guess Pokemon SoundUITests
+//
+//  Created by 紀韋如 on 06/05/2024.
+//
+
+import XCTest
+
+final class Guess_Pokemon_SoundUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
